33c7b4b192eb80ff9605632929c90e5adae8e2b5
VarunM <VarunM@hollywoodbets.net>
2021-07-02 18:07:08 +0200

    chore: test commit

--- AmazonProductFilter/Interfaces/IFilterCriteria.cs
+++ AmazonProductFilter/Interfaces/IFilterCriteria.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace AmazonProductFilter.Interfaces
 {
     public interface IFilterCriteria<T> where T : class
--- AmazonProductFilter/Interfaces/IProductFilter.cs
+++ AmazonProductFilter/Interfaces/IProductFilter.cs
@@ -1,7 +1,4 @@
-﻿using AmazonProductFilter.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace AmazonProductFilter.Interfaces
 {
